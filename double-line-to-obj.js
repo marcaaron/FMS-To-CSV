b0cd080fdcadbd24304c58f12083a310bc56fd62
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-06-21 15:18:06 -0700

    add space in description

--- double-line-to-obj.js
+++ double-line-to-obj.js
@@ -16,7 +16,7 @@ module.exports = (doubleLine) => {
       obj.orig_id = line1.substr(11,3).trim() || null;
       obj.qty = line1.substr(14,9).trim() || null;
       obj.subclass = line1.substr(29, 13).trim() || '';
-      obj.description = `${line1.substr(38, 30).trim() || ''}${line2.substr(23, 50).trim() || ''}`;
+      obj.description = `${line1.substr(38, 30).trim() || null} ${line2.substr(23, 50).trim() || null}`;
       obj.make = line1.substr(73,16).trim() || null;
       obj.model = line1.substr(95, 16).trim() || null;
       obj.serial_num = line1.substr(115).trim() || null;
