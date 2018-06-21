916cdafbf57417a587b978c895ddd1dbfd075d59
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-06-21 14:44:55 -0700

    dopeness

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 const fs = require('fs');
 const split = require('split');
 const jsonfile = require('jsonfile')
+const prompt = require('prompt');
+prompt.start();
+
+const schema = {
+  properties: {
+    response: {
+      pattern: /^[ynYN]$/,
+      message: 'Please Enter Y or N...',
+      required: true
+    }
+  }
+};
+
+// CSV Module
+
+const json2csv = require('./json2csv');
 
 // Parser Function
 const doubleLineToObj = require('./double-line-to-obj');
-
-// File Path
-const file = process.argv[2] || 'fms.txt';
 
 // Temp Variables
 const array = [];
 
 let previous = null;
-
 
 let district_code = null;
 let district_office = null;
@@ -22,47 +34,80 @@ let unit_code = null;
 let unit_name = null;
 let dtl = null;
 
-// Output File Path
-const output = './tmp/data.json';
+// File Path
+const file = process.argv[2] || 'fms.txt';
+const filename = file.replace(/\.txt/ig, '');
 
 // Read Stream
-fs.createReadStream(file)
-// Split on carriage return
-    .pipe(split(/\r/))
-    .on('data', function (current) {
-
-      if(/^DISTRICT OFFICE/.test(current)){
-        district_code = current.substr(16,9).trim() || null;
-        district_office = current.substr(21,49).trim() || null;
-        location = current.substr(79, 13).trim() || null;
-      }
-
-      if(/^SCHOOL\/UNIT/.test(current)){
-        unit_code = current.substr(18,7).trim() || null;
-        unit_name = current.substr(25,45).trim() || null;
-        dtl = current.substr(83, 9).trim() || null;
-      }
-
-      // If temp exists (a.k.a. we had a match on the last line)
-      // then push the previous line and current line into our array
-      if(previous){
-        array.push([previous, current, district_code, district_office, location, unit_code, unit_name, dtl]);
-        previous = null;
-      }
-
-      // Set previous to the current line if there's a match
-      previous = /^\d{10}/.test(current) && current;
-    })
-    .on('end', function(){
-      // Testing our temporary array of lines that begin with a
-      // property number and concatenating the next line...
-      array.forEach(dblLine=>{
-        doubleLineToObj(dblLine).then(res=>
-          jsonfile.writeFile(output, res, {spaces: 4, flag: 'a'}, function(err) {
-            // console.error(err)
-          })
-        );
-
+fs.stat(`./tmp/${filename}.csv`, function(err, stat) {
+  if(err == null) {
+      console.log(`A file named ${filename}.csv already exists.`);
+      console.log('Would You Like to Overwrite These Files? (Y/N)');
+      prompt.get(schema, function (err, result) {
+        if (err) { return onErr(err); }
+        if(/[yY]/.test(result.response)){
+          console.log('Overwriting Existing Files...');
+          fs.unlinkSync(`./tmp/${filename}.json`);
+          fs.unlinkSync(`./tmp/${filename}.csv`);
+          runScripts();
+        }else{
+          console.log('Goodbye!');
+        }
       });
-      console.log(`Created JavaScript Objects for ${array.length} entries.`);
+
+  } else if(err.code == 'ENOENT') {
+    runScripts();
+  } else {
+      console.log('Some other error: ', err.code);
+  }
+});
+
+function runScripts(){
+  console.log('Converting TXT to JSON...');
+  // Output File Path
+  const output = `./tmp/${filename}.json`;
+  fs.createReadStream(file)
+  // Split on carriage return
+  .pipe(split(/\r/))
+  .on('data', function (current) {
+
+    if(/^DISTRICT OFFICE/.test(current)){
+      district_code = current.substr(16,9).trim() || null;
+      district_office = current.substr(21,49).trim() || null;
+      location = current.substr(79, 13).trim() || null;
+    }
+
+    if(/^SCHOOL\/UNIT/.test(current)){
+      unit_code = current.substr(18,7).trim() || null;
+      unit_name = current.substr(25,45).trim() || null;
+      dtl = current.substr(83, 9).trim() || null;
+    }
+
+    // If temp exists (a.k.a. we had a match on the last line)
+    // then push the previous line and current line into our array
+    if(previous){
+      array.push([previous, current, district_code, district_office, location, unit_code, unit_name, dtl]);
+      previous = null;
+    }
+
+    // Set previous to the current line if there's a match
+    previous = /^\d{10}/.test(current) && current;
+  })
+  .on('end', function(){
+    // Testing our temporary array of lines that begin with a
+    // property number and concatenating the next line...
+    const arrayLength = array.length;
+    let count = 0;
+    array.forEach((dblLine)=>{
+      doubleLineToObj(dblLine).then(res=>
+        jsonfile.writeFile(output, res, {spaces: 4, flag: 'a'}, function(err) {
+          count++;
+          if(count===arrayLength){
+            json2csv(arrayLength, filename);
+            console.log('Converting JSON to CSV...');
+          }
+        })
+      );
     });
+  });
+}
--- json2csv.js
+++ json2csv.js
@@ -1,42 +1,64 @@
-const fs = require('fs');
-const Json2csvTransform = require('json2csv').Transform;
+module.exports = (entryCount, filename) => {
+  const fs = require('fs');
+  const Json2csvTransform = require('json2csv').Transform;
 
-// Json2csv Fields & Options
-const fields = [
-  'property_num',
-  'orig_id',
-  'qty',
-  'subclass',
-  'description',
-  'make',
-  'model',
-  'serial_num',
-  'acq',
-  'date',
-  'cost',
-  'fs',
-  'decal',
-  'po',
-  'prog_id',
-  'district_code',
-  'district_office',
-  'unit_code',
-  'unit_name',
-  'dtl',
-  'location',
-  'line1',
-  'line2' ];
+  // Json2csv Fields & Options
+  const fields = [
+    'property_num',
+    'orig_id',
+    'qty',
+    'subclass',
+    'description',
+    'make',
+    'model',
+    'serial_num',
+    'acq',
+    'date',
+    'cost',
+    'fs',
+    'decal',
+    'po',
+    'prog_id',
+    'district_code',
+    'district_office',
+    'unit_code',
+    'unit_name',
+    'dtl',
+    'location',
+    'line1',
+    'line2' ];
 
-const opts = { fields };
-const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
+  const opts = { fields };
+  const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
 
-// File Paths
-const inputPath = './tmp/data.json';
-const outputPath = './tmp/data.csv';
+  // File Paths
+  const inputPath = `./tmp/${filename}.json`;
+  const outputPath = `./tmp/${filename}.csv`;
 
-const input = fs.createReadStream(inputPath, { encoding: 'utf8' });
-const output = fs.createWriteStream(outputPath, { encoding: 'utf8' });
-const json2csv = new Json2csvTransform(opts, transformOpts);
+  const input = fs.createReadStream(inputPath, { encoding: 'utf8' });
+  const output = fs.createWriteStream(outputPath, { encoding: 'utf8' });
+  const json2csv = new Json2csvTransform(opts, transformOpts)
+  .on('end', () =>{
+      console.log(`-----------------------------------------------`);
+      console.log(`|      Successfully Converted TXT to CSV      |`);
+      console.log(`-----------------------------------------------`);
+      console.log(`|${' '.repeat(lgutter2)}Created ${count} entries!${' '.repeat(rgutter2)}|`);
+      console.log(`|${' '.repeat(lgutter)}${filename}.txt --> ${filename}.csv${' '.repeat(rgutter)}|`);
+      console.log(`-----------------------------------------------`);
+  });
 
-const processor = input.pipe(json2csv).pipe(output);
-console.log('done');
+  const processor = input.pipe(json2csv).pipe(output);
+  const ln = filename.length;
+  const gutter = (32 - (ln*2))/2;
+  let lgutter, rgutter;
+  lgutter = gutter;
+  rgutter = gutter;
+
+  const count = entryCount;
+  const entryGutter = (32 - (count.toString().length*2))/2;
+  let rgutter2 = entryGutter;
+  let lgutter2 = entryGutter;
+  if(entryGutter%2 !== 0){
+    rgutter2 +=1;
+  }
+}
