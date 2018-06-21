a9f06f7db5127869e9e6824c4ad0c585d0b77d7f
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-06-21 15:29:22 -0700

    break subclass

--- double-line-to-obj.js
+++ double-line-to-obj.js
@@ -15,8 +15,9 @@ module.exports = (doubleLine) => {
       obj.property_num = line1.substr(0,10).trim() || null;
       obj.orig_id = line1.substr(11,3).trim() || null;
       obj.qty = line1.substr(14,9).trim() || null;
-      obj.subclass = line1.substr(29, 13).trim() || '';
-      obj.description = `${line1.substr(38, 30).trim()} ${line2.substr(23, 50).trim()}`;
+      obj.subclass_id = line1.substr(29, 13).trim() || null;
+      obj.subclass_name = line1.substr(38, 30).trim() || null;
+      obj.description = line2.substr(23, 50).trim() || null;
       obj.make = line1.substr(73,16).trim() || null;
       obj.model = line1.substr(95, 16).trim() || null;
       obj.serial_num = line1.substr(115).trim() || null;
@@ -38,7 +39,7 @@ module.exports = (doubleLine) => {
 
       // Test to see if result has the correct number of keys 14 for no lines 16 if the lines are in
       const objectkeys = Object.keys(obj);
-        if(objectkeys.length !== 23){
+        if(objectkeys.length !== 24){
           console.log(obj, objectkeys.length);
         }
       resolve(obj);
